3cddef338dabb569fca9e136758cac0ccb594b71
liuze <liuzeyes@126.com>
2020-11-13 17:13:52 +0800

    阶段一

---
+++ 入门/dup1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		if input.Text() == "end" { break }
+		counts[input.Text()]++
+	}
+	// 注意：忽略input.Err()中可能的错误
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
