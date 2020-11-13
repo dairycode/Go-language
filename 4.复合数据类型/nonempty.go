b3ed56bc4ca7373de609803674ac20a35452f9d6
liuze <liuzeyes@126.com>
2020-11-13 19:37:49 +0800

    阶段二

---
+++ 4.复合数据类型/nonempty.go
@@ -0,0 +1,24 @@
+// Nonempty演示了slice的就地修改算法
+package main
+
+import "fmt"
+
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(data))
+	fmt.Printf("%q\n", data)
+}
+
+
+// nonempty返回一个新的 slice, slice中的元素都是非空字符串
+// 在函数的调用过程中，底层数组的元素发生了改变
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
